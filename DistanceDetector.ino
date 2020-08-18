295d24001bf734f6775867e3dc0c420a17d6b403
Russell <russellcape98@gmail.com>
2020-08-18 00:02:22 -0400

    added distance function and began corresponding the LEDs to the distance

--- DistanceDetector.ino
+++ DistanceDetector.ino
@@ -36,3 +36,39 @@ void setup() {
     pinMode(buzzer, OUTPUT)
 } // End of void setup().
 
+// Step 3: Create Main Loop
+
+void loop() {
+  /*Here, we are activating the Ultrasonic Sensor. The sensor sends out pulses of 
+  sound waves and uses them to measure the distance of an object (similar to a bats
+  echoloacation). This 'low' pulse creates delay so that the 'high' pulse gives a 
+  better signal.*/
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  // This 'high' pulse measures the distance of an object.
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // 'pulseIn' calculates the amount of time it took for the pulse to return and stores it in 'duration'.
+  duration = pulseIn(echoPin, HIGH);
+  // The distance is calculated here in cm.
+  distance = (duration/2) / 29.1;
+  // To calculate in inches, replace the above line with this:
+  // distance = (duration/2) / 74;
+
+  // This section of the code turns the LEDs on or off depending on the distance calculated.
+  if (distance <= 40) {
+    digitalWrite(white1, HIGH);
+    sound = 900;
+  }
+  else {
+    digitalWrite(white1, LOW);
+  }
+  if (distance < 30) {
+    digitalWrite(white2, HIGH);
+    sound = 1000;
+  }
+  else {
+    digitalWrite(white2, LOW);
+  }
+}
